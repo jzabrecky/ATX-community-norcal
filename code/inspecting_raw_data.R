7f3c903c7de01767211d6a5bb72e4fcb15ef66ce
jzabrecky <jordanmz@alumni.unc.edu>
2024-08-08 21:09:05 -0700

    finished adding TM data

---
+++ code/inspecting_raw_data.R
@@ -0,0 +1,44 @@
+#### TM data 2022
+
+library(tidyverse)
+# can full join to join everything
+
+# function to make reach a character
+change_df <- function(df) {
+  df$reach <- as.character(df$reach)
+  return(df)
+}
+
+# salmon 2022
+sal_1s_2022 <- read.csv("data/microscopy/raw/2022_TM_sal-1s.csv")
+sal_2_2022 <- change_df(read.csv("data/microscopy/raw/2022_TM_sal-2.csv"))
+sal_3_2022 <- change_df(read.csv("data/microscopy/raw/2022_TM_sal-3.csv"))
+
+tm_2022 <- full_join(sal_1s_2022, sal_2_2022)
+tm_2022 <- full_join(tm_2022, sal_3_2022)
+
+# south fork eel 2022
+sfe_1s_2022 <- read.csv("data/microscopy/raw/2022_TM_sfe-m-1s.csv")
+sfe_3_2022 <- change_df(read.csv("data/microscopy/raw/2022_TM_sfe-m-3.csv"))
+sfe_4_2022 <- change_df(read.csv("data/microscopy/raw/2022_TM_sfe-m-4.csv"))
+
+tm_2022 <- full_join(tm_2022, sfe_1s_2022)
+tm_2022 <- full_join(tm_2022, sfe_3_2022)
+tm_2022 <- full_join(tm_2022, sfe_4_2022)
+
+
+## checking 2023 TM data
+sal_1s_2023 <-read.csv("data/microscopy/raw/2023_TM_sal-1s.csv")
+sal_2_2023 <- change_df(read.csv("data/microscopy/raw/2023_TM_sal-3.csv"))
+sal_3_2023 <- change_df(read.csv("data/microscopy/raw/2023_TM_sal-3.csv"))
+
+tm_2023 <- full_join(sal_1s_2023, sal_2_2023)
+tm_2023 <- full_join(tm_2023, sal_3_2023)
+
+
+library(plyr)
+TM_samples <- ldply(list.files(path = "./data/microscopy/raw/", pattern = "TM"), function(filename) {
+  d <- read.csv(paste("data/microscopy/raw/", filename, sep = ""))
+  d$site_year = filename %>% stringr::str_remove("TM")
+  return(d)
+})
