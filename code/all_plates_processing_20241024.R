51b13681cfe45fceae3511b2ce3307cfe5882653
jzabrecky <jordanmz@alumni.unc.edu>
2024-10-24 18:33:29 -0700

    prelim looking at full green genes data

--- code/all_plates_processing_20241024.R
+++ code/all_plates_processing_20241024.R
@@ -1,52 +1,44 @@
-#### Assigning sample names and types to sequence reads
-### 9.4.2024
+## greengenes full sample analyses
 
-# NOTE: our "unknown" IDs from the plates are 108*, 801, 552, 554, and 931 
-# *there were 2 108s divided into 1000 and 1001
-# our "unknown" IDs from the sample metadata are 104, 115, 208, ??, ??
-# ?? are probably numbers > 500 and are unafiliated with this project
+# 10.24.2022
 
-# characteristics of unknown sample IDs:
-# 104 - Russian River, non-targeted/community composition (8/17/2022)
-# 115 - Russian River, targeted-anabaena/cylindrospermum (9/1/2022)
-# 208 - Russian River, non-targeted/community composition (9/15/2022)
-# ??? - Microcoleus rock from South Fork Eel (for another project) (SFE-M-1S, 9/8/22)
-# ??? - Control rock (green algae) from South Fork Eel (for another project) (SFE-M-1S, 9/8/22)
-
-# the known 108 is a RUS-3 fake TM from 8/17/2022
-
-# also have sample 27 which we were unsure if it was Microcoleus or not
-# (though I don't think it has a corresponding anatoxin concentration measurement bc sample was so small anyways)
+### loading libraries & data ###
 
 library(tidyverse)
+library(stringr)
 library(lubridate)
 
-## Read in data
-
-# taxonomy data -- replace with data including plate 3
-taxonomy <- read.csv("./data/molecular/Bac_counts_taxonomy_cleanedup.csv")
-
-# mutation from other code...
-Bac_counts_taxonomy<- taxonomy %>%
-  mutate(Phylum = gsub('_.*','',Phylum))
-
-Bac_counts_taxonomy<- Bac_counts_taxonomy %>%
-  mutate(Genus = gsub('_.*','',Genus))
-
-Bac_counts_taxonomy<- Bac_counts_taxonomy %>%
-  mutate(Genus = gsub('-.*','',Genus))
-
-# metadata
+# read in sample metadata
 metadata <- read.csv("./data/molecular/16s_sample_metadata.csv")
-# metadata <- read.csv("metadata.csv")
+
+# in this data, the sample number has already been reassigned to the plate
+data <- read.csv("data/molecular/mat_counts_unfiltered_all.csv")
+
+# separating out taxonomic ranks
+data$phylum <- str_match(data$Taxon, "p__\\s*(.*?)\\s*; c__")[,2]
+data$class <- str_match(data$Taxon, "c__\\s*(.*?)\\s*; o__")[,2]
+data$order <- str_match(data$Taxon, "o__\\s*(.*?)\\s*; f__")[,2]
+data$family <- str_match(data$Taxon, "f__\\s*(.*?)\\s*; g__")[,2]
+data$genus <- str_match(data$Taxon, "g__\\s*(.*?)\\s*; s__")[,2]
+data$species <- str_extract(data$Taxon, "(?<=s__).*")
+
+# get rid of columns we no longer care about
+data <- data %>% 
+  dplyr::select(!X & !feature_id & !Taxon)
+
+# get rid of counts = NA
+data <- data %>% 
+  filter(!is.na(counts))
 
 ## merge with metadata...
 metadata <- metadata %>% 
   rename(Sample_name = vial_ID) %>% 
   mutate(site_reach_date = paste(site_reach, field_date, sep = " "))
-all <- left_join(Bac_counts_taxonomy, metadata, by = "Sample_name")
+all <- left_join(data, metadata, by = "Sample_name")
 all$field_date <- mdy(all$field_date)
 all <- arrange(all, field_date, increasing = TRUE) # order by date for later plots
+
+#### looking at data!!
 
 # separating into groups based on river
 sfkeel <- all %>% 
@@ -68,27 +60,40 @@ rus_NT <- russian %>%
   filter(sample_type == "NT")
 
 ggplot(rus_NT, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Phylum)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = phylum)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # these should be reordered w/ time but too lazy to do that at present
+
+RUS_NT_cyano <- rus_NT %>% 
+  filter(phylum == "Cyanobacteria")
+
+ggplot(RUS_NT_cyano, aes(x = site_reach_date, y = counts)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 # see if any of our "fake" target samples actually contained microcoleus
 rus_fakes <- russian %>% 
   filter(sample_type == "TM") %>% 
-  filter(fake_target == "y") %>% 
-  filter(Phylum == "Cyanobacteriota")
+  filter(fake_target == "y")
 
 ggplot(rus_fakes, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Genus)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = phylum)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-# mostly synechococcus or something pink
+
+rus_fake_cyano <- rus_fakes %>% 
+  filter(phylum == "Cyanobacteria")
+
+ggplot(rus_fake_cyano, aes(x = site_reach_date, y = counts)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+# mostly synechococcus or something pink; two have lots of microcoleus actually
 rus_fakes_micro <- rus_fakes %>% 
-  filter(Genus == "Microcoleus")
+  filter(genus == "Microcoleus" & !is.na(counts))
 unique(rus_fakes_micro$site_reach_date)
 
 rus_fakes_metdata <- metadata %>% 
   filter(site == "RUS" & fake_target == "y")
-# so 6/15 of our "fake" samples actually did contain microcoleus
+# so 10/15 of our "fake" samples actually did contain microcoleus
 # "fake" meaning when we could not find microcoleus (which never happened on the Russian)
 # we took anything that looked like it could possibly be it (dark, in a riffle, etc.)
 # to confirm presence/absence
@@ -97,10 +102,10 @@ rus_fakes_metdata <- metadata %>%
 # target anabaena & cylindrospermum
 rus_anacylin <- russian %>% 
   filter(sample_type == "TAC") %>% 
-  filter(Phylum == "Cyanobacteriota")
+  filter(phylum == "Cyanobacteria")
 
 ggplot(rus_anacylin, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Genus)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # feel like I am missing samples here???
 # see note about Trichormus below... (is Anabaena essentially)
@@ -115,7 +120,7 @@ sfk_NT <- sfkeel %>%
   filter(sample_type == "NT")
 
 ggplot(sfk_NT, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Phylum)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = phylum)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # seeing a lot more cyanobacteria here than at the Russian!
 # again, feel like I'm missing samples
@@ -124,10 +129,13 @@ ggplot(sfk_NT, aes(x = site_reach_date, y = counts)) +
 sfk_TAC <- sfkeel %>% 
   filter(sample_type == "TAC") %>% 
   filter(fake_target == "n") %>% # need to specify fake here bc they took "fake" a week I was gone
-  filter(Phylum == "Cyanobacteriota")
+  filter(phylum == "Cyanobacteria")
+
+# might actually be worth it to look at family level instead, since that distinguishes
+# heterocysts
 
 ggplot(sfk_TAC, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Genus)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # lots of synnechoccus and trichormus which I don't think we saw a ton of
 # in our microscopy data (saw some synnechococcus but not a ton)
@@ -139,10 +147,10 @@ ggplot(sfk_TAC, aes(x = site_reach_date, y = counts)) +
 # TM, microcoleus
 sfk_TM <- sfkeel %>% 
   filter(sample_type == "TM") %>% 
-  filter(Phylum == "Cyanobacteriota")
+  filter(phylum == "Cyanobacteria")
 
 ggplot(sfk_TM, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Genus)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # mostly microcoleus!
 # early season sample is more mixed which makes sense
@@ -152,22 +160,26 @@ ggplot(sfk_TM, aes(x = site_reach_date, y = counts)) +
 # let's look at the one sample that was a maybe????
 sfk_TM_maybe <- sfk_TM %>% 
   filter(fake_target == "maybe")
-# okay this is one of the mysteriously missing samples
+
+ggplot(sfk_TM_maybe, aes(x = site_reach_date, y = counts)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+# mostly looks like anabaena samples so let's go with a no
 
 ## Lastly, the Salmon
 salmon_NT <- salmon %>% 
   filter(sample_type == "NT")
 
 ggplot(salmon_NT, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Phylum)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = phylum)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 salmon_TM <- salmon %>% 
   filter(sample_type == "TM") %>% 
-  filter(Phylum == "Cyanobacteriota") 
+  filter(phylum == "Cyanobacteria") 
 
 ggplot(salmon_TM, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Genus)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # mostly microcoleus! that last sample is a fake- I think it was mostly diatoms under the scope
 # when we looked at it back at the Angelo Reserve
@@ -176,10 +188,10 @@ ggplot(salmon_TM, aes(x = site_reach_date, y = counts)) +
 # we had one TAC sample
 salmon_TAC <- salmon %>% 
   filter(sample_type == "TAC") %>% 
-  filter(Phylum == "Cyanobacteriota")
+  filter(phylum == "Cyanobacteria")
 
 ggplot(salmon_TAC, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Genus)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # mostly trichormus like other TACs
 
@@ -191,31 +203,31 @@ sample_1000_108 <- all %>% # sample ID is 108 from plate but renamed to 1000 sin
   filter(Sample_name == 1000)
 
 ggplot(sample_1000_108, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Phylum)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = phylum)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # lots of cyanobacteria and proteobacteria; >60000 counts
 
 sample_1000_108_cyano <- sample_1000_108 %>% 
-  filter(Phylum == "Cyanobacteriota")
+  filter(phylum == "Cyanobacteria")
 
 ggplot(sample_1000_108_cyano, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Genus)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-# mostly synechoccus or trichormus??? almost 30,000 counts
+# mostly SIO2C1
 
 sample_1001_108 <- all %>% 
   filter(Sample_name == 1001)
 
 ggplot(sample_1001_108, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Phylum)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = phylum)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # more cyanobacteria and proteobacteria; >100,000 counts
 
-sample_1000_108_cyano <- sample_1001_108 %>% 
-  filter(Phylum == "Cyanobacteriota")
+sample_1001_108_cyano <- sample_1001_108 %>% 
+  filter(phylum == "Cyanobacteria")
 
-ggplot(sample_1000_108_cyano, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Genus)) + 
+ggplot(sample_1001_108_cyano, aes(x = site_reach_date, y = counts)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # mostly SIO2C1 (identified as Zarconia in cyanoseq- a member of oscillatoria)
 # more than 60000 counts
@@ -224,17 +236,17 @@ sample_801 <- all %>%
   filter(Sample_name == 801)
 
 ggplot(sample_801, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Phylum)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = phylum)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # more cyanobacteria and proteobacteria; almost 30000 reads
 
 sample_801_cyano <- sample_801 %>% 
-  filter(Phylum == "Cyanobacteriota")
+  filter(phylum == "Cyanobacteria")
 
 ggplot(sample_801_cyano, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Genus)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-# mostly trichormus??
+# mostly SIO2C1
 
 sample_552 <- all %>% 
   filter(Sample_name == 552) # sample not found
@@ -243,15 +255,15 @@ sample_554 <- all %>%
   filter(Sample_name == 554)
 
 ggplot(sample_554, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Phylum)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = phylum)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # mostly cyanobacteria; 15000 reads
 
 sample_554_cyano <- sample_554 %>% 
-  filter(Phylum == "Cyanobacteriota")
+  filter(phylum == "Cyanobacteria")
 
 ggplot(sample_554_cyano, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Genus)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # mostly tychonema which makes me think it is a TM or microcoleus rock
 # (probably meaghan's microcoleus rock)
@@ -260,43 +272,16 @@ sample_931 <- all %>%
   filter(Sample_name == 931)
 
 ggplot(sample_931, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Phylum)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = phylum)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # mostly cyanobacteria; not as diverse (i.e. less total phyla)
 
 sample_931_cyano <- sample_931 %>% 
-  filter(Phylum == "Cyanobacteriota")
+  filter(phylum == "Cyanobacteria")
 
 ggplot(sample_931_cyano, aes(x = site_reach_date, y = counts)) + 
-  geom_col(aes(x = site_reach_date, y = counts, fill = Genus)) + 
+  geom_col(aes(x = site_reach_date, y = counts, fill = genus)) + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 # okay this is almost certainly the microcoleus rock from the other study!!
-
-## okay stll weirded out that sample 552 seems to be missing
-## we have two that are like microcoleus rocks lol
-
-# need to look at ther samples (552 and 554) that are missing to confirm
-
-#### SEEMINGLY MISSING SAMPLES THAT SHOULD NOT BE MISSING
-all_sample_list <- as.data.frame(unique(all$Sample_name))
-colnames(all_sample_list) <- c("vial_ID")
-all_sample_list$test <- "yes"
-
-# testing each plate to see if we are missing a plate
-plate_1 <- read.table("./data/molecular/plate1_metadata.txt")
-colnames(plate_1) <- c("plate_ID", "vial_ID")
-test1 <- left_join(plate_1, all_sample_list, by = "vial_ID")
-# only NA for the two IDs that both have 108 so not an issue!
-# need to resolve above 1000 and 1001! (which is hopefully possible)
-
-plate_2 <- read.table("./data/molecular/plate2_metadata.txt")
-colnames(plate_2) <- c("plate_ID", "vial_ID")
-test2 <- left_join(plate_2, all_sample_list, by = "vial_ID")
-# this plate appears to be missing!!!
-
-plate_3 <- read.table("./data/molecular/plate3_metadata.txt")
-colnames(plate_3) <- c("plate_ID", "vial_ID")
-test3 <- left_join(plate_3, all_sample_list, by = "vial_ID")
-# vial ID 34 and 18 missing???
 
 #### NEED TO DEAL WITH TRIPLICATES AND BLANKS
